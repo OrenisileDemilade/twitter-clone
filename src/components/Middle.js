d1df9ecdc3283d0755c1a8de499970dde2f37cdf
OrenisileDemilade <dorenisile@gmail.com>
2022-09-04 23:42:14 +0100

    yo

---
+++ src/components/Middle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function middle() {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default middle
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SideBar.css'
+
+function Sidebar() {
+  return (
+    <div className='sideBar'>
+       
+    </div>
+  )
+}
+
+export default Sidebar
